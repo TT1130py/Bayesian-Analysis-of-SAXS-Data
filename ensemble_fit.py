7bccecfe0ce616faf7c90a764fab8bc4640a4051
tjaglal <jaglaltimothy@gmail.com>
2026-03-25 11:57:26 -0400

    progress of weighted average curve- need to access weights by structure name then complete equation

--- ensemble_fit.py
+++ ensemble_fit.py
@@ -3,21 +3,48 @@ import numpy as np
 import pandas as pd
 
 path_exp_file = "/home/malab/Desktop/BioEn-master/examples/scattering/files/experimental_data"
-sim_path = "/home/malab/Documents/sim_curves_test.txt"
+sim_path = "/home/malab/iBME/run_2025-11-20_0/run_summary/run_info_2025-11-20_0/structure_weights_sorted_2025-11-20_0.txt"
+structure_path = "/home/malab/iBME/pepsi_output"
 
 def simulated_curves(sim_file):
-    sim_pd = pd.DataFrame(pd.read_csv(sim_file, header=None))
+    sim_pd = pd.read_csv(sim_file, header=None)
+    sim_pd[2] = sim_pd[2].str.replace('.pdb','',regex=False)
 
+    all_weights = sim_pd[1]
     curves = len(sim_pd)
+    all_sim_s = []
+    all_sim_iq = []
+    filenames = []
 
-    file = sim_pd.iloc[0,0]
-    read_file = pd.DataFrame(pd.read_csv(f"{file}", sep=r"\s+", skiprows=5))
-    s_sim = read_file.iloc[:,0] * 10
-    iq_sim = read_file.iloc[:,1]
+    for i in range(curves):
+        if "mm" in sim_pd.iloc[i,2]:
+            file = sim_pd.iloc[i,2]
+        else:
+            continue
+        read_file = pd.read_csv("{}/{}.out".format(structure_path, file), sep=r"\s+", skiprows=5)
+        s_sim = read_file.iloc[:,0] * 10
+        iq_sim = read_file.iloc[:,1]
+
+        all_sim_s.append(s_sim)
+        all_sim_iq.append(iq_sim)
+        filenames.append(file)
+
+    df_s = pd.DataFrame(all_sim_s, index=filenames)
+    df_iq = pd.DataFrame(all_sim_iq, index=filenames)
+
+    fixed_weights = all_weights.drop(all_weights.index[0])
+    df_weights = pd.concat([fixed_weights, pd.DataFrame(filenames)], axis=1)
 
     length = s_sim.iloc[-1]
 
-    return length, s_sim, iq_sim, file
+    return length, df_s, df_iq, file, df_weights, all_weights
+
+def average_curve(s_array, iq_array):
+    s_values = s_array.iloc[0, :]
+    iq_tran = iq_array.transpose()
+    merge = pd.concat([s_values, iq_tran], axis=1)
+
+    merge.columns.values[0] = "s"
 
 def experimental_curve(path_exp_file, sim_length):
     exp_pd = pd.DataFrame(pd.read_csv("{}/SASDLU4.dat".format(path_exp_file),
@@ -67,7 +94,23 @@ def plot_compare(s_sim, iq_sim, s, iq, err, s_full, iq_full, err_full, f_name):
 #for i in range(curves-1):
 
 
-len, sangle, intense, file_name = simulated_curves(sim_path)
-angletrun, intensetrun, errtrun, anglefull, intensefull, errfull = experimental_curve(path_exp_file, len)
-plot_compare(sangle, intense, angletrun, intensetrun, errtrun, anglefull, intensefull, errfull, file_name)
+len, sangle, intense, file_name, weights, wlist = simulated_curves(sim_path)
+#angletrun, intensetrun, errtrun, anglefull, intensefull, errfull = experimental_curve(path_exp_file, len)
+#plot_compare(sangle, intense, angletrun, intensetrun, errtrun, anglefull, intensefull, errfull, file_name)
+
+s_values = sangle.iloc[0, :]
+iq_tran = intense.transpose()
+merge = pd.concat([s_values, iq_tran], axis=1)
+
+merge.columns.values[0] = "s"
+for i in range(2):
+    if "mm" in merge.columns[i]:
+        structure = merge.columns[i]
+    else:
+        continue
+
+    condition = weights[1].str.contains(f"{structure}", na=False)
+    w = weights.loc[condition, 0]
+
+
 print("Breakpt")
