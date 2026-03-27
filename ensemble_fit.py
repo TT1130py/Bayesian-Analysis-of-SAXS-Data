d0606e638d4bbf103b0eb398708b0dabaa8f40ee
tjaglal <jaglaltimothy@gmail.com>
2026-03-27 09:39:53 -0400

    complete

--- ensemble_fit.py
+++ ensemble_fit.py
@@ -2,9 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+##########------ PLOT SAXS CURVE COMPARISON OF EXPERIMENT VS WEIGHTED AVERAGE SIMULATION
+
+#path to the experimental SAXS data
 path_exp_file = "/home/malab/Desktop/BioEn-master/examples/scattering/files/experimental_data"
+#path to the "structure weights sorted" file that contains structure file name and its weight (output from iBME)
 sim_path = "/home/malab/iBME/run_2025-11-20_0/run_summary/run_info_2025-11-20_0/structure_weights_sorted_2025-11-20_0.txt"
+#path to the simulated SAXS curves of above files
 structure_path = "/home/malab/iBME/pepsi_output"
+
+##########------ FUNCTIONS
 
 def simulated_curves(sim_file):
     sim_pd = pd.read_csv(sim_file, header=None)
@@ -39,12 +46,40 @@ def simulated_curves(sim_file):
 
     return length, df_s, df_iq, file, df_weights, all_weights
 
-def average_curve(s_array, iq_array):
+def average_curve(s_array, iq_array, weights):
     s_values = s_array.iloc[0, :]
     iq_tran = iq_array.transpose()
     merge = pd.concat([s_values, iq_tran], axis=1)
 
     merge.columns.values[0] = "s"
+    weighted_merge = merge.copy()
+
+    for i in range(len(merge.columns)):
+        if "mm" in merge.columns[i]:
+            structure = merge.columns[i]
+        else:
+            continue
+
+        match = weights.loc[weights["structure file"] == structure, "weights"]
+        if match.empty:
+            w = 0
+        else:
+            w = match.iloc[0]
+            if pd.isna(w):
+                w = 0
+
+        for x in range(len(merge)):
+            iq_i = merge.loc[x, structure]
+            weighted_merge.loc[x, structure] = iq_i * w
+
+    averaged_merge = pd.DataFrame({"s": s_values, "iq": np.nan})
+    for i in range(len(merge)):
+        series = weighted_merge.iloc[i, 1:]
+        series_np = np.array(series)
+        average = np.sum(series_np)
+        averaged_merge.loc[i, "iq"] = average
+
+    return averaged_merge.loc[:,"s"], averaged_merge.loc[:,"iq"]
 
 def experimental_curve(path_exp_file, sim_length):
     exp_pd = pd.DataFrame(pd.read_csv("{}/SASDLU4.dat".format(path_exp_file),
@@ -91,38 +126,21 @@ def plot_compare(s_sim, iq_sim, s, iq, err, s_full, iq_full, err_full, f_name):
     ax_2.set_title("Simulated SAXS fit with Experiment - full")
     ax.legend()
     plt.show()
-#for i in range(curves-1):
 
+##########------ MAIN
 
-lent, sangle, intense, file_name, weights, wlist = simulated_curves(sim_path)
-#angletrun, intensetrun, errtrun, anglefull, intensefull, errfull = experimental_curve(path_exp_file, len)
-#plot_compare(sangle, intense, angletrun, intensetrun, errtrun, anglefull, intensefull, errfull, file_name)
+def main():
+    #Find all simulated SAXS curves and place in dataframe
+    lent, sangle, intense, file_name, weights_df, wlist = simulated_curves(sim_path)
 
-s_values = sangle.iloc[0, :]
-iq_tran = intense.transpose()
-merge = pd.concat([s_values, iq_tran], axis=1)
+    #Perform the weighted average SAXS curve
+    s_weighted, iq_weighted = average_curve(sangle, intense, weights_df)
 
-merge.columns.values[0] = "s"
-weighted_merge = merge.copy()
+    #Create the SAXS curves from experimental data for plotting
+    angletrun, intensetrun, errtrun, anglefull, intensefull, errfull = experimental_curve(path_exp_file, lent)
 
-for i in range(len(merge.columns)):
-    if "mm" in merge.columns[i]:
-        structure = merge.columns[i]
-    else:
-        continue
+    #Plot experiment vs weighted average simulation
+    plot_compare(s_weighted, iq_weighted, angletrun, intensetrun, errtrun, anglefull, intensefull, errfull, file_name)
 
-    match = weights.loc[weights["structure file"] == structure, "weights"]
-    if match.empty:
-        w = 0
-    else:
-        w = match.iloc[0]
-        if pd.isna(w):
-            w = 0
-
-    for x in range(len(merge)):
-        iq_i = merge.loc[x, structure]
-        weighted_merge.loc[x,structure] = iq_i * w
-
-
-
+main()
 print("Breakpt")
