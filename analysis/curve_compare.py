d5bc98e1d4eaafb475bfdcba0feb583ad82d5522
tjaglal <jaglaltimothy@gmail.com>
2026-05-27 11:45:36 -0400

    IN PROGRESS- compare SAXS curves between different datasets

---
+++ analysis/curve_compare.py
@@ -0,0 +1,11 @@
+#####----- Compare SAXS curves between multiple sets of reweighed structures
+
+#####----- IMPORTS
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import glob
+from natsort import natsorted
+
+#####----- PATHS, ARGUMENTS, REQUIREMENTS
