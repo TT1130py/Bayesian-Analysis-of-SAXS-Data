d7845f905fe57b49790b6cd34a5d6a99c2983ba5
tjaglal <jaglaltimothy@gmail.com>
2026-06-10 10:50:03 -0400

    calculate radius of gyration for each curve and plot as additional legend

--- analysis/curve_compare.py
+++ analysis/curve_compare.py
@@ -8,6 +8,8 @@ import os
 import glob
 from natsort import natsorted
 import seaborn as sns
+from scipy.stats import linregress
+import matplotlib.lines as mlines
 
 #####----- PATHS, ARGUMENTS, REQUIREMENTS
 experimental_data = "/users/t/j/tjaglal/experimental_data/SASDLU4.dat"
@@ -53,6 +55,24 @@ def experimental_curve(path_exp_file, sim_length, save_path):
 
     return s_trun, iq_trun, err_trun
 
+def rg_guinier(s, iq, fit_points):
+    valid_idx = iq > 0
+    s_valid = s[valid_idx]
+    iq_valid = iq[valid_idx]
+
+    s_low = s_valid[:fit_points]
+    iq_low = iq_valid[:fit_points]
+
+    x = s_low**2
+    y = np.log(iq_low)
+
+    slope, intercept, r_value, p_value, std_err = linregress(x, y)
+    if slope >= 0:
+        print("Positive slope")
+        return np.nan, r_value**2
+    rg = np.sqrt(-3 * slope)
+
+    return rg, r_value**2
 def match_files(sim_file, save_path, dro, r0):
     sim_pd = pd.read_csv(sim_file, sep='\\t', header=0)
     sim_pd["PDB_Name"] = sim_pd["PDB_Name"].str.replace('.pdb', '', regex=False)
@@ -108,7 +128,7 @@ def VACC_average_curve(sim_file, pdb_names, s_val, iq_val):
     return len(sim_merge), sim_merge.iloc[:, 0], sim_merge.iloc[:, 1], sim_merge.iloc[:, 2]
     print("Breakpt")
 
-def plot_curves(s_sim_arr, iq_sim_arr, iq_prior_arr, s, iq, err):
+def plot_curves(s_sim_arr, iq_sim_arr, iq_prior_arr, s, iq, err, post_rg_arr, pri_rg_arr, exp_rg):
     colors_post = sns.color_palette("flare", n_colors=wam)
     colors_pri = sns.color_palette("crest", n_colors=wam)
 
@@ -124,7 +144,17 @@ def plot_curves(s_sim_arr, iq_sim_arr, iq_prior_arr, s, iq, err):
     ax.set_ylabel("i(q)")
     ax.set_xlabel("s")
     ax.set_title("Posterior SAXS curves with experiment")
-    ax.legend()
+
+    leg_post = ax.legend(loc="upper right")
+    ax.add_artist(leg_post)
+
+    rg_handles_post = [mlines.Line2D([], [], color='none', label=f"Exp Rg: {exp_rg:.2f} Å")]
+    for i in range(wam):
+        label_text = fr"$\delta \rho$: {dro_list[i]} | $r_0$: {r0_list[i]} Rg: {post_rg_arr[i]:.2f} Å"
+        rg_handles_post.append(mlines.Line2D([], [], color='none', label=label_text))
+
+    ax.legend(handles=rg_handles_post, loc="lower left", title="Radius of Gyration ($R_g$)", handlelength=0,
+              handletextpad=0)
 
     fig_2, ax_2 = plt.subplots(figsize=(10, 10))
     ax_2.errorbar(s, iq, yerr=err, fmt='o', markersize=3, ecolor="lightgray", label="Experiment")
@@ -138,21 +168,50 @@ def plot_curves(s_sim_arr, iq_sim_arr, iq_prior_arr, s, iq, err):
     ax_2.set_ylabel("i(q)")
     ax_2.set_xlabel("s")
     ax_2.set_title("Prior SAXS curves with experiment")
-    ax_2.legend()
+
+    leg_pri = ax_2.legend(loc="upper right")
+    ax_2.add_artist(leg_pri)
+
+    # 2nd Legend (Rg Values)
+    rg_handles_pri = [mlines.Line2D([], [], color='none', label=f"Exp Rg: {exp_rg:.2f} Å")]
+    for i in range(wam):
+        label_text = fr"$\delta \rho$: {dro_list[i]} | $r_0$: {r0_list[i]} Rg: {pri_rg_arr[i]:.2f} Å"
+        rg_handles_pri.append(mlines.Line2D([], [], color='none', label=label_text))
+
+    ax_2.legend(handles=rg_handles_pri, loc="lower left", title="Radius of Gyration ($R_g$)", handlelength=0,
+                handletextpad=0)
 
     return fig, fig_2
+
 def main():
     s_arr = []
     wiq_arr = []
     piq_arr = []
+    pos_rg_arr = []
+    pri_rg_arr = []
+
+    lent = 1000
+    #Will be overwritten for real plotting
+    angletrun, intensetrun, errtrun = experimental_curve(experimental_data, lent)
+    exp_rg, exp_r2 = rg_guinier(angletrun, intensetrun, 15)
+
     for i in range(wam):
         s, concat_merge, weights, f_name = match_files(post_weights_list[i], save_paths_list[i], dro_list[i], r0_list[i])
         lent, s_weighted, iq_weighted, iq_prior = VACC_average_curve(post_weights_list[i], f_name, s, concat_merge)
         s_arr.append(s_weighted)
         wiq_arr.append(iq_weighted)
         piq_arr.append(iq_prior)
+
+        post_rg, post_r2 = rg_guinier(s_weighted, iq_weighted, 15)
+        prior_rg, prior_r2 = rg_guinier(s_weighted, iq_prior, 15)
+        pos_rg_arr.append(post_rg)
+        pri_rg_arr.append(prior_rg)
+
     angletrun, intensetrun, errtrun = experimental_curve(experimental_data, lent)
-    post_figure, pri_figure = plot_curves(s_arr, wiq_arr, piq_arr, angletrun, intensetrun, errtrun)
+    post_figure, pri_figure = plot_curves(s_arr, wiq_arr, piq_arr, angletrun, intensetrun, errtrun, exp_rg, pos_rg_arr, pri_rg_arr)
 
     post_figure.savefig(f"{output_path}/posterior_saxs_curves.png", dpi=300, bbox_inches='tight')
     pri_figure.savefig(f"{output_path}/prior_saxs_curves.png", dpi=300, bbox_inches='tight')
+
+if __name__ == '__main__':
+    main()
