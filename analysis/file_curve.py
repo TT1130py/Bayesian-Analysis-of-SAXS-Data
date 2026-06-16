e4eb5b218e42759d87f9ceed96eb0bdc9c1ba15e
tjaglal <jaglaltimothy@gmail.com>
2026-06-16 12:08:39 -0400

    functional script

--- analysis/file_curve.py
+++ analysis/file_curve.py
@@ -1,3 +1,5 @@
+from os import name
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -5,14 +7,14 @@ import os
 import seaborn as sns
 
 #####----- PATHS, ARGUMENTS, REQUIREMENTS
-path_exp_file = ""
-search_path = ""
-save_path = ""
+path_exp_file = "/home/malab/Desktop/BioEn-master/examples/scattering/files/experimental_data/"
+search_path = "/home/malab/Desktop/out_files"
+save_path = "/home/malab/Downloads"
 
-files = []
+files = ["mm016_016", "mm064_742", "mm016_003", "mm016_021", "mm016_698", "mm016_048", "mm256_832"]
 #####----- FUNCTIONs
 
-def experimental_curve(path_exp_file, sim_length, save_path):
+def experimental_curve(path_exp_file, sim_length):
     exp_pd = pd.DataFrame(pd.read_csv("{}/SASDLU4.dat".format(path_exp_file),
                                       header=None, sep=r"\s+"))
 
@@ -24,41 +26,37 @@ def experimental_curve(path_exp_file, sim_length, save_path):
     iq_trun = iq.iloc[:sim_length].values
     err_trun = err.iloc[:sim_length].values
 
-    plt.errorbar(s, iq, yerr=err, fmt='o', ecolor="lightgray", markersize=3)
-    plt.yscale("log")
-    plt.ylabel("i(q)")
-    plt.xlabel("s")
-    plt.title("Experimental SAXS curve with x log")
-    #save_path_1 = "{}/experiment.png".format(save_path)
-    #plt.savefig(save_path, dpi=300)
-
     return s_trun, iq_trun, err_trun
 
 def find_curves(files, search_path):
     q = []
     iq = []
+    column_names = ["q", "Iq exp", "dl exp", "Iq fit"]
     for i in range(len(files)):
-        pdb = pd.read_csv(f"{search_path}/{files[i]}.out", sep=r"\s+", skiprows=5, header=0)
-        pdb.columns = pdb.columns.str.lstrip("#")
+        pdb = pd.read_csv(f"{search_path}/{files[i]}-SASDLU4.fit", sep=r"\s{2,}", skiprows=5, header=0, comment="#", names=column_names, engine="python")
+        pdb.columns = pdb.columns.str.lstrip("#").str.strip()
 
-        q_np = pdb["q"].to_numpy()
-        iq_np = pdb["I"].to_numpy()
+        q_np = pdb["q"].to_numpy() * 10
+        iq_np = pdb["Iq fit"].to_numpy()
 
         q.append(q_np)
         iq.append(iq_np)
 
-    lent = len(q_np[0])
+    lent = len(q_np)
     return lent, q, iq
 
 def plot_curves(s, iq, err, files, q_arr, iq_arr):
-    colors = sns.color_palette("crest", n_colors=len(files))
+    colors = sns.color_palette("bright", n_colors=len(files))
 
     fig, ax = plt.subplots(figsize = (10,10))
     ax.errorbar(s, iq, yerr = err, fmt= 'o', markersize=3, ecolor="lightgray", label="Experiment")
     ax.set_yscale("log")
 
     for i in range(len(files)):
-        ax.plot(q_arr[0], iq_arr[i], zorder= i+1, lw=3, label= files[i], color=colors[i])
+        sim_iq = iq_arr[i]
+        scale = np.sum(iq * sim_iq) / np.sum(sim_iq ** 2)
+        scaled_sim_iq = sim_iq * scale
+        ax.plot(q_arr[i], scaled_sim_iq, zorder= i+2, lw=3, label= files[i], color=colors[i])
     ax.set_ylabel("i(q)")
     ax.set_xlabel("s")
     ax.set_title("SAXS curves vs Experiment")
@@ -69,7 +67,17 @@ def plot_curves(s, iq, err, files, q_arr, iq_arr):
 
 def main():
     length, q_files, iq_files = find_curves(files, search_path)
-    s_exp, iq_exp, err_exp = experimental_curve(path_exp_file, length, save_path)
-    plot_curves(s_exp, iq_exp, err_exp, files, q_files, iq_files)
+    s_exp, iq_exp, err_exp = experimental_curve(path_exp_file, length)
+    reference_q = q_files[0]
 
+    #iq_exp_interp = np.interp(reference_q, s_exp, iq_exp)
+    #err_exp_interp = np.interp(reference_q, s_exp, err_exp)
+
+    # 5. Plot using the synchronized reference_q
+    plot_curves(
+        s_exp, iq_exp, err_exp, files, q_files, iq_files
+    )
+
+if __name__ == "__main__":
+    main()
 
