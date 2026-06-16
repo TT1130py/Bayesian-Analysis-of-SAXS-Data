2aaa07ab66a277d7f8ef6d2402ae522c2fbc84c9
tjaglal <jaglaltimothy@gmail.com>
2026-06-16 15:46:47 -0400

    CLI initialization

--- analysis/Rg_gps.py
+++ analysis/Rg_gps.py
@@ -1,14 +1,5 @@
 #####----- Compare radius of gyration distributions between multiple sets of reweighed structures
 
-#This script will compare radius of gyration values across different grid point simulations for both prior and posterior weights
-#Each gridpoint dataset requires a number of arguments:
-    #Path to the posterior weights file
-    #Path to the GRID sum file
-    #Path to the simulated saxs curves directory
-    #d_rho and r0 values
-        #All of these variables must additionally be added into the arrays located right above the FUNCTIONS section
-#The wam variable is the number of data subsets utilized in a specific run
-
 #####----- IMPORTS
 import numpy as np
 import pandas as pd
@@ -17,42 +8,8 @@ import glob
 from natsort import natsorted
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-#####----- PATHS, ARGUMENTS, REQUIREMENTS
-experimental_data = "/path/to/experimental/file"
-experimental_rg = 6.7 * 10 #convert to angstrom
-output_path = ''
-
-## Edit here and/or add another subset for every weight file used
-post_weights_1 = ""
-grid_sum_1 = "" #this is grid sum
-save_path_1 = ""
-best_dro_1 = ""
-best_r0_1 = ""
-
-## Edit here and/or add another subset for every weight file used
-post_weights_2 = ""
-grid_sum_2 = "" #this is grid sum
-save_path_2 = ""
-best_dro_2 = "" #int
-best_r0_2 = "" #int
-
-## Edit here and/or add another subset for every weight file used
-post_weights_3 = ""
-grid_sum_3 = "" #this is grid sum
-save_path_3 = ""
-best_dro_3 = "" #int
-best_r0_3 = "" #int
-
-## Amount of weights used
-wam = "" #int
-
-## Add new variables to list
-best_dro = [best_dro_1]
-best_r0 = [best_r0_1]
-post_weights = [post_weights_1]
-grid_paths = [grid_sum_1]
-save_paths = [save_path_1]
+import yaml
+import argparse
 
 #####----- FUNCTIONS
 def concat_rg(best_dro, best_r0, grid_path, save_path):
@@ -99,14 +56,13 @@ def concat_rg(best_dro, best_r0, grid_path, save_path):
 
     return rg_df
 
-def init_plot(rg_df_list, post_weights):
+def init_plot(rg_df_list, post_weights, best_dro, best_r0, wam, experimental_rg):
 
     #Posterior plot
     post_fig, post_ax = plt.subplots(figsize = (10,10))
     sns.set_style("ticks")
     colors_post = sns.color_palette("flare", n_colors=wam)
     colors_pri = sns.color_palette("crest", n_colors=wam)
-    
     for i in range(wam):
         a = 0.9
         post_weights_ind = pd.read_csv(post_weights[i], sep='\s+')
@@ -121,7 +77,7 @@ def init_plot(rg_df_list, post_weights):
         rg_sim_post = merged['Rg'].to_numpy()
         post_weights_arr = merged['weight'].to_numpy()
 
-        sns.kdeplot(x=rg_sim_post, weights=post_weights_arr, color=colors_post[i], label=fr'$\delta \rho$: {best_dro[i]} | $r_0$: {best_r0[i]}', ax=post_ax)
+        sns.kdeplot(x=rg_sim_post, weights=post_weights_arr, color=colors_post[i], label=fr'$\delta \rho$: {best_dro[i]} | $r_0$: {best_r0[i]}', linewidth= a, ax=post_ax)
         a -= 0.1
     post_ax.axvline(x=experimental_rg, color='green', linestyle='--', label='Experimental Rg')
 
@@ -138,7 +94,7 @@ def init_plot(rg_df_list, post_weights):
         a_2 = 0.9
         rg_sim_prior = rg_df_list[i]['Rg'].to_numpy()
         prior_weights = np.ones(len(rg_sim_prior)) / len(rg_sim_prior)
-        sns.kdeplot(x=rg_sim_prior, weights=prior_weights, color=colors_pri[i], label=fr'$\delta \rho$: {best_dro[i]} | $r_0$: {best_r0[i]}', ax=pri_ax)
+        sns.kdeplot(x=rg_sim_prior, weights=prior_weights, color=colors_pri[i], label=fr'$\delta \rho$: {best_dro[i]} | $r_0$: {best_r0[i]}', linewidth=a_2, ax=pri_ax)
         a_2 -= 0.1
     pri_ax.axvline(x=experimental_rg, color='green', linestyle='--', label='Experimental Rg')
 
@@ -150,15 +106,48 @@ def init_plot(rg_df_list, post_weights):
     return post_fig, pri_fig
 
 def main():
+
+    #####----- Initialize CLI arguments and config
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", type=str, default="config.yaml")
+    args = parser.parse_parser_args() if hasattr(parser, 'parse_parser_args') else parser.parse_args()
+
+    with open(args.config, "r") as f:
+        master_config = yaml.safe_load(f)
+
+    rg_config = master_config.get("rg_gps", {})
+    if not rg_config:
+        print("rg_gps config not found")
+        return
+
+    experimental_data = rg_config.get("experimental_data", "")
+    experimental_rg = rg_config.get("experimental_rg", "")
+    output_path = rg_config.get("output_path", "")
+
+    best_dro = [rg_config.get("best_dro_1", ""), rg_config.get("best_dro_2", ""),]
+    best_r0 = [rg_config.get("best_r0_1", ""), rg_config.get("best_r0_2", ""),]
+    post_weights = [rg_config.get("post_weights_1", ""), rg_config.get("post_weights_2", ""),]
+    grid_paths = [rg_config.get("grid_sum_1", ""), rg_config.get("grid_sum_2", ""),]
+    save_paths = [rg_config.get("save_path_1", ""), rg_config.get("save_path_2", ""),]
+
+    wam = len([x for x in post_weights if x not in ["", None]])
+    if wam == 0:
+        print("No weights found")
+        return
+
+    #####----- Run main script
     rg_df_list = []
     for i in range(wam):
+        print(f"Processing run {i+1}/{wam}...")
         rg_df = concat_rg(best_dro[i], best_r0[i], grid_paths[i], save_paths[i])
         rg_df_list.append(rg_df)
 
-    posterior_plot, prior_plot = init_plot(rg_df_list, post_weights)
-    posterior_plot.savefig(f"{output_path}/posterior_rg_distribution.png", dpi=300, bbox_inches='tight')
-    prior_plot.savefig(f"{output_path}/prior_rg_distribution.png", dpi=300, bbox_inches='tight')
+    print("Generating plots...")
+    posterior_plot, prior_plot = init_plot(rg_df_list, post_weights, best_dro, best_r0, wam, experimental_rg)
+    posterior_plot.savefig(f"{output_path}/posterior_rg_distribution_ref.png", dpi=300, bbox_inches='tight')
+    prior_plot.savefig(f"{output_path}/prior_rg_distribution_ref.png", dpi=300, bbox_inches='tight')
 
+    print("Success!")
 if __name__ == '__main__':
     main()
 
